e6db4cbf2ed5c47d01edd89d5806901e284d89a1
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-27 03:52:30 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array(array, element) = ("new_element")
   add_element_to_end_of_array(array, element) << "new_element"
 end
-
+end
 def add_element_to_start_of_array(array, element)
   #add_element_to_start_of_array(array, element).unshift
 end
